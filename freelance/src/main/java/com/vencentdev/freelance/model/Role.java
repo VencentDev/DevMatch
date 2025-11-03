0673befd4e0134e180cbfcef74760ace892901e7
Vencent Delute <iceiceicebell@gmail.com>
2025-11-03 14:47:55 +0800

    authentication test

--- freelance/src/main/java/com/vencentdev/freelance/model/Role.java
+++ freelance/src/main/java/com/vencentdev/freelance/model/Role.java
@@ -12,10 +12,19 @@ public class Role {
     @Column(unique = true, nullable = false)
     private String name;
 
-    public Role() {}
-    public Role(String name) { this.name = name; }
+    public Role(){}
 
-    public Long getId() { return id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public Role(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public Long getId(){
+        return id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
 }
--- freelance/src/main/java/com/vencentdev/freelance/model/User.java
+++ freelance/src/main/java/com/vencentdev/freelance/model/User.java
@@ -1,6 +1,8 @@
 package com.vencentdev.freelance.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,9 +17,16 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     @Column(unique = true, nullable = false)
     private String username;
 
+    @NotBlank
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotBlank
     @Column(nullable = false)
     private String password;
 
@@ -28,14 +37,20 @@ public class User implements UserDetails {
     private Set<Role> roles = new HashSet<>();
 
     public User() {}
-    public User(String username, String password, Set<Role> roles) {
+
+    // Full constructor requiring email
+    public User(String username, String email, String password, Set<Role> roles) {
         this.username = username;
+        this.email = email;
         this.password = password;
-        this.roles = roles;
+        if (roles != null) {
+            this.roles = roles;
+        }
     }
 
     public Long getId() { return id; }
     public void setUsername(String username) { this.username = username; }
+    public void setEmail(String email) { this.email = email; }
     public void setPassword(String password) { this.password = password; }
     public Set<Role> getRoles() { return roles; }
     public void setRoles(Set<Role> roles) { this.roles = roles; }
@@ -54,6 +69,8 @@ public class User implements UserDetails {
     @Override
     public String getUsername() { return username; }
 
+    public String getEmail() { return email; }
+
     @Override
     public boolean isAccountNonExpired() { return true; }
 
@@ -65,4 +82,17 @@ public class User implements UserDetails {
 
     @Override
     public boolean isEnabled() { return true; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
