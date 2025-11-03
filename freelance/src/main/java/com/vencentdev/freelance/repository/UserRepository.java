48646a21726e1869fc2a9c978691f7637245d7e8
Vencent Delute <iceiceicebell@gmail.com>
2025-11-03 16:51:42 +0800

    users can use either email or username to login

--- freelance/src/main/java/com/vencentdev/freelance/repository/UserRepository.java
+++ freelance/src/main/java/com/vencentdev/freelance/repository/UserRepository.java
@@ -2,8 +2,19 @@ package com.vencentdev.freelance.repository;
 
 import com.vencentdev.freelance.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+
+    // Resolve by either username OR email (useful for "login with email or username")
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    // Convenience checks
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
