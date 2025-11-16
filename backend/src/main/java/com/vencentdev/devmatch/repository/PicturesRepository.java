3b396d0dd37c224cc6f81da28f1255cec541fe87
Vencent Delute <iceiceicebell@gmail.com>
2025-11-16 14:30:19 +0800

    fix pictures

--- backend/src/main/java/com/vencentdev/devmatch/repository/PicturesRepository.java
+++ backend/src/main/java/com/vencentdev/devmatch/repository/PicturesRepository.java
@@ -1,9 +1,12 @@
-package com.vencentdev.devmatch.service;
+package com.vencentdev.devmatch.repository;
+
+import com.vencentdev.devmatch.model.Pictures;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PicturesRepository extends JpaRepository<Pictures, Long> {
-  Optional<Pictures> findByUserId(Long userId);
-
-  void updateByUser(User user);
-  void deleteByUser(User user);
+    Optional<Pictures> findByUserId(Long userId);
 }
